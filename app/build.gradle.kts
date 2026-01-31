70c5a759a9b3f5f66b9e9dd9aa986ce956d0bc37
Arsalan Khan <arsalanbinkhan@gmail.com>
2026-01-31 12:00:55 +0530

    Arsalan And Zain Implemented

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -2,7 +2,7 @@ plugins {
     id("com.android.application")
     id("org.jetbrains.kotlin.android")
     id("com.google.gms.google-services")
-
+    id("com.google.firebase.crashlytics")
 }
 
 android {
@@ -17,7 +17,6 @@ android {
         versionName = "1.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
-
     }
 
     buildTypes {
@@ -40,11 +39,15 @@ android {
         sourceCompatibility = JavaVersion.VERSION_1_8
         targetCompatibility = JavaVersion.VERSION_1_8
     }
+
+    kotlinOptions {
+        jvmTarget = "1.8"
+    }
 }
 
 dependencies {
 
-    // ================= ANDROIDX CORE =================
+    // ===== ANDROIDX =====
     implementation("androidx.core:core-ktx:1.12.0")
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.11.0")
@@ -53,11 +56,11 @@ dependencies {
     implementation("androidx.cardview:cardview:1.0.0")
     implementation("androidx.activity:activity-ktx:1.8.0")
 
-    // ================= LIFECYCLE =================
+    // ===== LIFECYCLE =====
     implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.6.2")
     implementation("androidx.lifecycle:lifecycle-livedata-ktx:2.6.2")
 
-    // ================= FIREBASE (BoM - FIXED) =================
+    // ===== FIREBASE (BoM) =====
     implementation(platform("com.google.firebase:firebase-bom:32.5.0"))
 
     implementation("com.google.firebase:firebase-auth")
@@ -66,52 +69,52 @@ dependencies {
     implementation("com.google.firebase:firebase-storage")
     implementation("com.google.firebase:firebase-database")
     implementation("com.google.firebase:firebase-messaging")
-    implementation("com.google.firebase:firebase-crashlytics")
     implementation("com.google.firebase:firebase-perf")
     implementation("com.google.firebase:firebase-config")
+    implementation("com.google.firebase:firebase-crashlytics")
 
-    // ================= GOOGLE MAPS & AUTH =================
+    // ===== GOOGLE MAPS =====
     implementation("com.google.android.gms:play-services-maps:18.2.0")
     implementation("com.google.android.gms:play-services-location:21.0.1")
-    implementation("com.google.android.gms:play-services-auth:20.7.0")
+    implementation("com.google.android.gms:play-services-auth:21.0.0")
 
-    // ================= NETWORKING =================
+    // ===== NETWORK =====
     implementation("com.squareup.okhttp3:okhttp:4.12.0")
     implementation("com.google.code.gson:gson:2.10.1")
     implementation("org.json:json:20231013")
     implementation("com.squareup.retrofit2:retrofit:2.9.0")
     implementation("com.squareup.retrofit2:converter-gson:2.9.0")
 
-    // ================= IMAGE LOADING =================
+    // ===== IMAGE =====
     implementation("com.github.bumptech.glide:glide:4.16.0")
     annotationProcessor("com.github.bumptech.glide:compiler:4.16.0")
 
     implementation("com.google.maps.android:android-maps-utils:3.8.0")
 
-    // ================= PAYMENT =================
+    // ===== PAYMENT =====
     implementation("com.razorpay:checkout:1.6.38")
 
-    // ================= IMAGE COMPRESSION =================
+    // ===== COMPRESSION =====
     implementation("id.zelory:compressor:3.0.1")
 
-    // ================= UI EFFECTS =================
+    // ===== UI =====
     implementation("com.airbnb.android:lottie:6.3.0")
     implementation("com.facebook.shimmer:shimmer:0.5.0")
 
-    // ================= PERMISSIONS =================
+    // ===== PERMISSIONS =====
     implementation("com.karumi:dexter:6.2.3")
 
-    // ================= WORK MANAGER =================
+    // ===== WORK MANAGER =====
     implementation("androidx.work:work-runtime:2.9.0")
 
-    // ================= NAVIGATION =================
+    // ===== NAVIGATION =====
     implementation("androidx.navigation:navigation-fragment:2.7.7")
     implementation("androidx.navigation:navigation-ui:2.7.7")
 
-    // ================= QR CODE =================
+    // ===== QR =====
     implementation("com.journeyapps:zxing-android-embedded:4.3.0")
 
-    // ================= TESTING =================
+    // ===== TESTING =====
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
